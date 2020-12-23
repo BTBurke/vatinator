80fd28e990f0ad73d18a3f5e0b7ad87c8bc00d42
Bryan Burke <bryan.t.burke@gmail.com>
2020-12-23 09:11:41 +0200

    detects more vendor formats

--- ocr/vendor.go
+++ ocr/vendor.go
@@ -2,12 +2,17 @@ package ocr
 
 import (
 	"regexp"
+	"strings"
 )
 
 var v *regexp.Regexp
+var v2 *regexp.Regexp
 
 func init() {
+	// company symbol at end
 	v = regexp.MustCompile(`[^/,]+\s(AS|TÜ|UÜ|OÜ|As|Tü|Uü|Oü|OU|Ou|TU|Tu|UU|Uu|0Ü|0u|0U|0ü)`)
+	// company symbol at front
+	v2 = regexp.MustCompile(`(AS|TÜ|UÜ|OÜ|As|Tü|Uü|Oü|OU|Ou|TU|Tu|UU|Uu|0Ü|0u|0U|0ü)\s[^/,]+$`)
 }
 
 type vendor struct{}
@@ -28,13 +33,28 @@ func VendorRule() Rule {
 	return vendor{}
 }
 
-// extracts the vendor name from the receipt
 func extractVendor(lines []string) string {
+	regexes := []*regexp.Regexp{v, v2}
+	for _, r := range regexes {
+		out := extract(r, lines)
+		if len(out) > 0 {
+			return finalFixes(out)
+		}
+	}
+	return ""
+}
+
+// extracts the vendor name from the receipt
+func extract(r *regexp.Regexp, lines []string) string {
 	for _, line := range lines {
-		c := v.FindAllStringSubmatch(line, -1)
+		c := r.FindAllStringSubmatch(line, -1)
 		if len(c) > 0 && len(c[0][0]) > 0 {
 			return c[0][0]
 		}
 	}
 	return ""
+}
+
+func finalFixes(s string) string {
+	return strings.ReplaceAll(s, "0", "O")
 }
---
+++ ocr/vendor_test.go
@@ -0,0 +1,23 @@
+package ocr
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVendor(t *testing.T) {
+	tt := []struct {
+		name string
+		in   string
+		out  string
+	}{
+		{name: "company at front", in: "OU APRANGA Estonia", out: "OU APRANGA Estonia"},
+		{name: "with symbols", in: "H&M Hennes & Mauritz OÜ", out: "H&M Hennes & Mauritz OÜ"},
+		{name: "misdetected 0 for O", in: "Test Company 0U", out: "Test Company OU"},
+	}
+	for _, tc := range tt {
+		out := extractVendor([]string{tc.in})
+		assert.Equal(t, tc.out, out)
+	}
+}
