db8f404933b50d63d14d93681d654df28ea04331
Bryan Burke <bryan.t.burke@gmail.com>
2020-12-28 14:08:08 +0200

    start refactor

---
+++ process.go
@@ -0,0 +1,17 @@
+package vat
+
+import "github.com/BTBurke/vatinator/svc"
+
+type Options struct {
+	CredentialPath string
+	ZipResult      bool
+	OutputPath     string
+	Hooks          svc.Hooks
+	FormData       FormData
+}
+
+// Process will read receipts located at path and process them into VAT and excise forms.  Returned
+// values are full paths to every file generated.
+func Process(path string) (string, error) {
+
+}
