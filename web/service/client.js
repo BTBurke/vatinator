5bc39a7328270b15ab1b41a099c1cf8261796fdf
Bryan Burke <bryan.t.burke@gmail.com>
2020-12-31 13:00:11 +0200

    update upstream url for testing

--- web/service/client.js
+++ web/service/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const Client = () => {
   const options = {};  
-  options.baseURL = 'http://localhost:8080';
+  options.baseURL = 'https://api.vatinator.com';
   options.withCredentials = true;
   const instance = axios.create(options);  
   return instance;
