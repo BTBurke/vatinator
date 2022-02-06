390e8f07a2ac2f360b448aaafd7bd37a17e2959a
Bryan Burke <bryan.t.burke@gmail.com>
2022-02-06 11:48:20 +0200

    updates date handling for 2022

--- ocr/date_test.go
+++ ocr/date_test.go
@@ -12,18 +12,19 @@ func TestDates(t *testing.T) {
 		in   string
 		out  string
 	}{
-		{"dot delimited full year", "09.12.2020", "09/12/2020"},
-		{"dot delimited short year", "09.12.20", "09/12/2020"},
-		{"spaces and extra info", "date: 09.12.20", "09/12/2020"},
-		{"slash delimited full year", "09/12/2020", "09/12/2020"},
-		{"slash delimited short year", "09/12/20", "09/12/2020"},
-		{"not delimited full year", "09122020", "09/12/2020"},
-		{"not delimited short year", "091220", "09/12/2020"},
-		{"not delimited full year 21", "09122021", "09/12/2021"},
-		{"spaces inside year", "09.12. 2020", "09/12/2020"},
-		{"hyphen delimited", "09-12-2020", "09/12/2020"},
-		{"pathological from ocr fail", "29.12, 2020", "29/12/2020"},
-		// TODO: only handles dates through 2021
+		// 2022 update
+		{"dot delimited full year", "09.12.2022", "09/12/2022"},
+		{"dot delimited short year", "09.12.22", "09/12/2022"},
+		{"spaces and extra info", "date: 09.12.22", "09/12/2022"},
+		{"slash delimited full year", "09/12/2022", "09/12/2022"},
+		{"slash delimited short year", "09/12/22", "09/12/2022"},
+		{"not delimited full year", "09122022", "09/12/2022"},
+		{"not delimited short year", "091222", "09/12/2022"},
+		{"not delimited full year 22", "09122022", "09/12/2022"},
+		{"spaces inside year", "09.12. 2022", "09/12/2022"},
+		{"hyphen delimited", "09-12-2022", "09/12/2022"},
+		{"pathological from ocr fail", "29.12, 2022", "29/12/2022"},
+		// TODO: only handles dates through 2022
 	}
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
@@ -39,9 +40,9 @@ func TestReverseDates(t *testing.T) {
 		in   string
 		out  string
 	}{
-		{"reversed date", "2020-12-29", "29/12/2020"},
-		{"reversed date short year", "20-12-29", "29/12/2020"},
-		// TODO: only handles dates through 2021
+		{"reversed date", "2021-12-29", "29/12/2021"},
+		{"reversed date short year", "21-12-29", "29/12/2021"},
+		// TODO: only handles dates through 2022
 	}
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
